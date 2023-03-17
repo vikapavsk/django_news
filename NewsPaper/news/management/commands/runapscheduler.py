c0e090d091b05e27dfcc36453635eb04efbca5f0
vikapavsk <vikavinograd1@gmail.com>
2023-03-17 15:34:15 +0300

    new files

---
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -0,0 +1,87 @@
+import logging
+from datetime import datetime, timedelta
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django_apscheduler import util
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from news.models import Post, Category
+from news.signals import *
+
+
+logger = logging.getLogger(__name__)
+
+def my_job():
+    today = datetime.now()
+    last_week = today - timedelta(days=7)
+    posts = Post.objects.filter(dateCreation__gte=last_week)
+    categories = set(posts.values_list('category__name', flat=True))
+    subscribers = set(Category.objects.filter(name__in=categories).values_list('subscribers__email', flat=True))
+    html_content = render_to_string(
+        'weekly_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Посты за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+@util.close_old_connections
+def delete_old_job_executions(max_age=604_800):
+    """
+    This job deletes APScheduler job execution entries older than `max_age`
+    from the database.
+    It helps to prevent the database from filling up with old historical
+    records that are no longer useful.
+
+    :param max_age: The maximum length of time to retain historical
+                    job execution records. Defaults to 7 days.
+    """
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+    help = "Runs APScheduler."
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            my_job,
+            trigger=CronTrigger(day_of_week="fri", hour="18", minute="00"),  # Every 10 seconds
+            id="my_job",  # The `id` assigned to each job MUST be unique
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job 'my_job'.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="fri", hour="18", minute="00"
+            ),
+            id="delete_old_job_executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added weekly job: 'delete_old_job_executions'.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
